3cc700fe3686b941d0e2a29d95dda5aeb4ca4e5c
szekeres miklos <hiimmikescott@gmail.com>
2024-01-09 11:43:31 +0100

    Teszt3

--- src/test/java/TombsugTest.java
+++ src/test/java/TombsugTest.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.monitor.Tombsug;
@@ -5,9 +6,12 @@ import com.monitor.Tombsug;
 public class TombsugTest {
 
     Tombsug tombsug;
-    
+    @BeforeTest
+    public void setUp(){
+        this.tombsug = new Tombsug();
+    }
     @Test
     public void testCalcRadius_30_50(){
-
+        this.tombsug.calcRadius();
     }
 }
