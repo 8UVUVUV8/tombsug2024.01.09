1e5478ed768b50f8bf3c91a7398425efd79c9be1
szekeres miklos <hiimmikescott@gmail.com>
2024-01-09 12:07:07 +0100

    Teszt12

--- src/main/java/com/monitor/Tombsug.java
+++ src/main/java/com/monitor/Tombsug.java
@@ -1,7 +1,12 @@
 package com.monitor;
 
+import java.util.InputMismatchException;
+
 public class Tombsug {
     public double calcRadius(double side, double angle){
+        if (side <= 0) {
+            throw new InputMismatchException();
+        }
         double rad = angle * Math.PI / 180;
         double r;
         r = 1.0/2.0 * side * Math.sin(rad);
