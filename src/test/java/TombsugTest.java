d935d816d9e9c651a011dcf8ffa573f98653b742
Bandus <sz.andras2400@gmail.com>
2024-01-09 19:35:44 +0100

    kaczuratnezve

--- src/test/java/TombsugTest.java
+++ src/test/java/TombsugTest.java
@@ -33,4 +33,38 @@ public class TombsugTest {
     public void testCalcRadius_Zero_Angle(){
         this.tombsug.calcRadius(0, 0);
     }
+    @Test
+    public void tesztTeszt(){
+        //System.out.println("teszt");
+        int a = 2, b = 3;
+        int res = a+b;
+        int exp = 5;
+        Assert.assertEquals(res, exp);
+    }
+    @Test
+    public void tesztTeszt2(){
+        int szam = 2;
+        Double szam2 = new Double(""+szam);
+        Integer szam3 = 2;
+        //if (szam2 instanceof Integer) {
+        //    System.out.println("Ez egy szam");
+        //}
+        Assert.assertEquals(szam2, szam3*1.0);
+    }
+    @Test
+    public void tesztTeszt4(){
+        Integer szam1 = 2;
+        Integer szam2 = new Integer(2);
+        String szoveg1 = "1";
+        String szoveg2 = "2";
+
+        if (szam1 instanceof Integer) {
+            szoveg1 = "Integer";
+        }
+
+        if (szam2 instanceof Integer) {
+            szoveg2 = "Integer";
+        }
+        Assert.assertEquals(szoveg1, szoveg2);
+    }
 }
